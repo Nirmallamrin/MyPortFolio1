610af968ac6edd4cc0b35c6d7b32f8a7946d90c0
[nirmal] <nirmallamrin77@gmail.com>
2024-12-05 19:14:01 +0530

    projects  added

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -5,50 +5,69 @@ import weather from "../assets/IMG41.png"
 import todo from "../assets/IMG42.png"
 import expense from "../assets/IMG43.png"
 import nuxt from "../assets/IMG44.png"
+import blogs from "../assets/IMG13.png"
+import estate from "../assets/IMG12.png";
+
 const projects = [
   {
-    title: 'E-Commerce Web Application',
-    description: 'A full-featured e-commerce web application built using the MERN stack.',
+    title: "E-Commerce Web Application",
+    description:
+      "A full-featured e-commerce web application built using the MERN stack.",
     imageUrl: news,
-    liveLink: 'https://e-commerce-fe-cyan.vercel.app/',
-    githubLink: 'https://github.com/Nirmallamrin',
+    liveLink: "https://e-commerce-fe-cyan.vercel.app/",
+    githubLink: "https://github.com/Nirmallamrin",
   },
   {
-    title: 'News Website',
-    description: 'A news website that aggregates articles from various sources, using MERN Stack',
-    imageUrl:ecommerce ,
-    liveLink: 'https://entri-news-client.vercel.app/',
-    githubLink: 'https://github.com/Nirmallamrin',
+    title: "News Website",
+    description:
+      "A news website that aggregates articles from various sources, using MERN Stack",
+    imageUrl: ecommerce,
+    liveLink: "https://entri-news-client.vercel.app/",
+    githubLink: "https://github.com/Nirmallamrin",
+  },
+  {
+    title: "Estate WebSite",
+    description: "Estate Website Using MERN Stack",
+    imageUrl: estate,
+    liveLink: "https://estate-web-mp5d.vercel.app/",
+    githubLink: "https://github.com/Nirmallamrin/EstateWeb.git",
   },
   {
     title: "Weather App",
     description: " Weather App using React Vite",
     imageUrl: weather,
-    liveLink: 'https://weather-app-dun-kappa-84.vercel.app/',
-    githubLink: 'https://github.com/Nirmallamrin/WeatherApp.git',
+    liveLink: "https://weather-app-dun-kappa-84.vercel.app/",
+    githubLink: "https://github.com/Nirmallamrin/WeatherApp.git",
   },
   {
     title: "To Do List ",
     description: "To Do List App using React Vite",
     imageUrl: todo,
-    liveLink: 'https://to-do-list-seven-beta-43.vercel.app/',
-    githubLink: 'https://github.com/Nirmallamrin/To-Do-List.git',
+    liveLink: "https://to-do-list-seven-beta-43.vercel.app/",
+    githubLink: "https://github.com/Nirmallamrin/To-Do-List.git",
   },
   {
     title: "Expense Tracker",
     description: "Expense Tracker App using React Vite",
     imageUrl: expense,
-    liveLink: 'https://expense-tracker-rouge-ten.vercel.app/',
-    githubLink: 'https://github.com/Nirmallamrin/ExpenseTracker.git',
+    liveLink: "https://expense-tracker-rouge-ten.vercel.app/",
+    githubLink: "https://github.com/Nirmallamrin/ExpenseTracker.git",
   },
   {
     title: "Nuxt Portfolio",
     description: "My Portfolio using Nuxt.js",
     imageUrl: nuxt,
-    
-    githubLink: 'https://github.com/Nirmallamrin/nuxtPortfolio.git',
+
+    githubLink: "https://github.com/Nirmallamrin/nuxtPortfolio.git",
   },
-  
+
+  {
+    title: "Blog",
+    description: "Blog Web Application Using Next JS",
+    imageUrl: blogs,
+
+    githubLink: "https://github.com/Nirmallamrin/blog-app-next.git",
+  },
 ];
 
 const Projects = () => {
